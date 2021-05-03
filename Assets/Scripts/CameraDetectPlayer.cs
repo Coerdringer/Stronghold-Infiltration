cf035db540ba38a8ec6de99d863b3a494c753f60
Piotr Trzmiel <t.pomal@gmail.com>
2021-05-04 01:10:28 +0200

    few terrains, hill on them with a texture, continuou turn/move, snap turn added to XR Rig

---
+++ Assets/Scripts/CameraDetectPlayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraDetectPlayer : MonoBehaviour
+{
+    Renderer m_Renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_Renderer.isVisible)
+        {
+
+        }
+    }
+}
